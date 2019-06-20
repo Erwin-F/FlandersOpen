0d256054ff0ea8be1adcbe6f763886e6ffc270eb
Erwin François <erwin.francois@gmail.com>
2019-06-20 23:01:08 +0200

    portal project

--- FlandersOpen.Portal/Program.cs
+++ FlandersOpen.Portal/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace FlandersOpen.React
+namespace FlandersOpen.Portal
 {
     public class Program
     {
